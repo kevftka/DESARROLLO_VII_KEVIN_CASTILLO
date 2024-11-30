ea06984964e6243ed35dd239d414938a1a7ea175
Kevin Castillo <kevftka@MacBook-Pro-de-Kevin.local>
2024-11-29 20:07:47 -0500

    sdfa

--- Parciales/PARCIAL_4/config.php
+++ Parciales/PARCIAL_4/config.php
@@ -1,5 +1,12 @@
 <?php
+<<<<<<< HEAD
 define('CLIENT_ID', '');
 define('CLIENT_SECRET', '');
 define('REDIRECT_URI', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
 ?>
+=======
+define('CLIENT_ID', '943567472806-dq86lvjlehmka5g5ela1a4u0tkg8s6df.apps.googleusercontent.com');
+define('CLIENT_SECRET', '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');
+define('REDIRECT_URI', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
+?>
+>>>>>>> 71081da (update repo)
