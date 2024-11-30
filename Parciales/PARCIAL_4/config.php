24df64181d95ca4b21b4e4eac87b805eb3d29489
Kevin Castillo <38598471+kevftka@users.noreply.github.com>
2024-11-29 20:36:58 -0500

    asd

--- Parciales/PARCIAL_4/config.php
+++ Parciales/PARCIAL_4/config.php
@@ -1,19 +1,18 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 define('CLIENT_ID', '');
 define('CLIENT_SECRET', '');
 define('REDIRECT_URI', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
 ?>
-=======
+<?php
 define('CLIENT_ID', '943567472806-dq86lvjlehmka5g5ela1a4u0tkg8s6df.apps.googleusercontent.com');
 define('CLIENT_SECRET', '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');
 define('REDIRECT_URI', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
 ?>
->>>>>>> 71081da (update repo)
-=======
+
+<?php
 define('CLIENT_ID', '');
 define('CLIENT_SECRET', '');
 define('REDIRECT_URI', 'http://localhost/PARCIALES/PARCIAL_4/callback.php');
 ?>
->>>>>>> 1a9bf40 (parcial4)
+
